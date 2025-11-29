47621176e24f5b42d12b66c5d69f0f6ff35b55ca
Eric Bighi - Home <ericmbighi@gmail.com>
2025-11-29 09:39:49 -0300

    feat: adjust aside menu layout

--- Poc/frontend/menu/components/Menu.vue
+++ Poc/frontend/menu/components/Menu.vue
@@ -1,18 +1,48 @@
+<script setup lang="ts">
+interface MenuItem {
+  label: string;
+  icon: string;
+  href: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    label: "Usuários",
+    icon: "fas fa-users fa-2x",
+    href: "/configuracao/usuarios",
+  },
+  {
+    label: "Configurações",
+    icon: "fas fa-cog fa-2x",
+    href: "/configuracao",
+  },
+  {
+    label: "Relatórios",
+    icon: "fas fa-chart-bar fa-2x",
+    href: "/relatorios",
+  },
+];
+</script>
+
 <template>
   <div class="flex h-screen">
     <aside
-      class="group bg-gray600 text-white py-5 space-y-4 hidden md:block
-             w-[80px] hover:w-[270px] transition-all duration-300 overflow-hidden">
-      <nav class="flex flex-col space-y-2 px-2">
-        <a href="/configuracao/usuarios"
-           class="group flex items-center gap-3 py-4 rounded-lg
-                  text-gray-300 hover:bg-gray-700 transition-all duration-200">
-          <i class="fas fa-users fa-2x group-hover:text-blue-500 transition-colors duration-200"></i>
-          <h2
-            class="mb-0 whitespace-nowrap opacity-0 group-hover:opacity-100
-                   group-hover:text-blue-500 transition-all duration-300">
-            Usuários
-          </h2>
+      class="group bg-gray600 text-white py-3 space-y-4
+             w-[70px] hover:w-[240px] transition-all duration-300 overflow-hidden">
+      <nav class="flex flex-col space-y-2">
+         <a
+          v-for="item in menuItems"
+          :key="item.href"
+          :href="item.href"
+          class="group flex items-center gap-3 m-0 p-3
+                 text-gray300 hover:bg-gray700 transition-all duration-200">
+
+          <i :class="item.icon + ' group-hover:text-blue transition-colors duration-200'"></i>
+
+          <h3 class="mb-0 whitespace-nowrap opacity-0 group-hover:opacity-100
+                     group-hover:text-blue transition-all duration-300">
+            {{ item.label }}
+          </h3>
         </a>
       </nav>
     </aside>
